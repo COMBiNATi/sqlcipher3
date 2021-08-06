d08795e7953a27277d6e9c02e68ade0eb1586245
Łukasz Stolcman <lukasz.stolcman@tivix.com>
2021-08-06 10:11:25 -0700

    Fix build script

--- build-scripts/build.ps1
+++ build-scripts/build.ps1
@@ -12,7 +12,12 @@ Remove-Item sqlcipher.zip
 # assume you have vs build tools, otherwise, correct the path
 # https://stackoverflow.com/a/2124759
 pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools"
-cmd /c "VsDevCmd.bat&set" | Out-Null
+cmd /c "VsDevCmd.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
 popd
 Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
 
